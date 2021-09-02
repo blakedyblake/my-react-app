467eb08d5203d4893627f12582798cd310521191
Blake Nuttall <blakedyblake@gmail.com>
2021-09-02 16:45:07 -0600

    Trying out deployment...

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+
+const app = express()
+//The site didn't say to say this
+app.use(express.json())
+app.use(cors())
+
+//Wack
+const PublicPath = path.join(__dirname, '..', 'public')
+app.use(express.static(PublicPath))
+
+//Wack
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(PublicPath, 'index.html'))
+})
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, ()=>{
+    console.log(`Running on port ${PORT}`)
+})
